6ed448bdf350195f49ee2eaf734f33f4f2ec06f1
Shreyansh Kumar <shreyan.kumar29@gmail.com>
2026-08-15 00:53:09 +0530

    Initial Commits

--- First-Class-Functions/lecture.js
+++ First-Class-Functions/lecture.js
@@ -1,9 +1,32 @@
+// function square(x)
+// {
+//     return x * x
+// }
+
+// var f = square
+
+// console.log(square)
+// console.log(f(5))
+
+
 function square(x)
 {
     return x * x
 }
 
-var f = square(5)
+function my_map(func, arr)
+{
+    result = []
+    for (var i = 0; i < arr.length; i++)
+    {
+        result.push(func(arr[i]))
+    }
+    return result
+}
 
-console.log(square)
-console.log(f)
+var squares= my_map(square, [1, 2, 3, 4, 5]) 
+console. log(squares) 
+function cube(x) 
+{
+    return x * x * x; 
+}
--- First-Class-Functions/lecture.py
+++ First-Class-Functions/lecture.py
@@ -1,8 +1,25 @@
+# def square(x):
+#     return x ** 2
+
+# ## Removing the Parenthesis and treating f as a function
+# f = square
+
+# print(f(5))
+
+# print(square)
+
 def square(x):
     return x ** 2
 
-f = square(5)
+def my_map(func, arg_list):
+    result = []
+    for i in arg_list:
+        result.append(func(i))
+    return result
 
-print(f)
+squares = my_map(square, [1, 2, 3, 4, 5])
 
-print(square)
+print(squares)
+
+def cube(x):
+    return x ** 3
